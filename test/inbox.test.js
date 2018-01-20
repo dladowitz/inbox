e66246cb53b42c46ce132eca3c662d1e3186e041
David Ladowitz <david@ladowitz.com>
2018-01-20 12:47:53 -0800

    add method tests

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,31 +1,76 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
 
-class Car {
-  park(){
-    return 'stopped';
-  }
-
-  drive(){
-    return 'vroom';
-  }
-}
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 
-let car;
+const { interface, bytecode } = require('../compile.js');
 
-beforeEach(() => {
-  car = new Car();
-})
+let accounts;
+let inbox;
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
+
+beforeEach(async () => {
+  // Get list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+
+  // Use one of the accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there'] })
+    .send({ from: accounts[0], gas: '1000000'})
+
+  inbox.setProvider(provider);
+});
+
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address)
   });
 
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  })
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal('Hi there', message);
+  });
+
+  it('can set a message', async () => {
+    await inbox.methods.setMessage('Bye there').send({ from: accounts[0], gas: '1000000'});
+    assert.equal('Bye there', await inbox.methods.message().call());
+  });
 });
+
+
+
+
+
+
+
+// class Car {
+//   park(){
+//     return 'stopped';
+//   }
+//
+//   drive(){
+//     return 'vroom';
+//   }
+// }
+//
+//
+// let car;
+//
+// beforeEach(() => {
+//   car = new Car();
+// })
+//
+// describe('Car', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+//
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   })
+// });
